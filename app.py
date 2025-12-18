b61f1b1f501bb167c8b01ab8896a8b8b57a7270d
benji641 <c3015335@newcastle.ac.uk>
2025-12-18 15:39:48 +0000

    database update

--- app.py
+++ app.py
@@ -19,9 +19,47 @@ class Post(db.Model):
     __tablename__ = "posts"
 
     id = db.Column(db.Integer, primary_key=True)
+
+    userid = db.Column(
+        db.Integer,
+        db.ForeignKey(
+            'users.id',
+            name='fk_posts_userid_users'
+        ),
+        nullable=False
+    )
+
     created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     title = db.Column(db.Text, nullable=False)
     body = db.Column(db.Text, nullable=False)
+
+    user = db.relationship("User", back_populates="posts")
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(100), nullable=False)
+
+    posts = db.relationship("Post", back_populates="user")
+
+    __table_args__ = (
+        db.UniqueConstraint('email', name='uq_users_email'),
+    )
+
+    def __init__(self, email, firstname, lastname, phone, password):
+        self.email = email
+        self.firstname = firstname
+        self.lastname = lastname
+        self.phone = phone
+        self.password = password
+
 
 # Blueprints
 app.register_blueprint(accounts_bp)
